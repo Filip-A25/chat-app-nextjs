920cc3145a6a5ab9a73bd0b054d27f26c2a143b0
Filip-A25 <filip.agatic25@gmail.com>
2024-07-30 12:54:37 +0200

    Add logic and styling for error page

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import errorIcon from "@/assets/error-icon.png";
+import { loggedState } from "@/app/authentication/state";
+import { useRecoilValue } from "recoil";
+import { paths } from "@/app/types";
+
 export default function ErrorPage() {
+  const isLoggedIn = useRecoilValue(loggedState);
+
   return (
-    <section>
-      <h1>ERROR: Something went wrong...</h1>
+    <section className="h-full flex flex-col justify-center items-center max-sm:px-4">
+      <div className="flex items-center">
+        <Image
+          src={errorIcon}
+          alt="Error Icon"
+          className="w-[85px] md:w-[100px] pr-3"
+        />
+        <h1 className="text-2xl md:text-3xl">
+          <b>ERROR:</b> Something went wrong...
+        </h1>
+      </div>
+      <Link
+        href={isLoggedIn ? paths.myChats : paths.home}
+        className="max-sm:mt-5 text-lg text-red-500"
+      >
+        {isLoggedIn ? "Go to my chats." : "Go to homepage."}
+      </Link>
     </section>
   );
 }
